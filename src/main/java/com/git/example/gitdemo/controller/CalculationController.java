cc48672a365b9e028c7b731af8cea20b5aba3d54
Syed <remindme7899@gmail.com>
2024-09-30 21:17:57 +0530

    committing controller calculate

---
+++ src/main/java/com/git/example/gitdemo/controller/CalculationController.java
@@ -0,0 +1,20 @@
+package com.git.example.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+
+@RestController
+@RequestMapping("/calculate")
+public class CalculationController {
+	
+	
+	@PostMapping("/add")
+	public int add(@RequestParam int n1, @RequestParam int n2, @RequestParam int n3) {
+		return n1+n2+n3;
+	}
+
+}
